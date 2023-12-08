49014e95e4a7122f3e534e33b47cce0042046af9
Gegelman Roman Yurievich <gegelmanr@mail.ru>
2023-12-09 00:31:26 +0700

    passwords table was added

--- webNET-Hits-backend-aspnet-project-2/Data/AppDbContext.cs
+++ webNET-Hits-backend-aspnet-project-2/Data/AppDbContext.cs
@@ -6,6 +6,7 @@ namespace webNET_Hits_backend_aspnet_project_2.Data
     public class AppDbContext : DbContext
     {
         public DbSet<UserData> Users { get; set; }
+        public DbSet<PasswordModel> UserPasswords { get; set; }
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
         {
---
+++ webNET-Hits-backend-aspnet-project-2/Migrations/AppDbContext/20231208173102_Passwords.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webNET_Hits_backend_aspnet_project_2.Migrations
+{
+    /// <inheritdoc />
+    public partial class Passwords : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserPasswords",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    HashedPassword = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserPasswords", x => x.UserId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserPasswords");
+        }
+    }
+}
