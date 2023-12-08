bdb6277cc6ac640a0160ec58d55e5537ab90f927
Gegelman Roman Yurievich <gegelmanr@mail.ru>
2023-12-09 00:34:17 +0700

    posts were added
    made model and migration

--- webNET-Hits-backend-aspnet-project-2/Data/AppDbContext.cs
+++ webNET-Hits-backend-aspnet-project-2/Data/AppDbContext.cs
@@ -7,6 +7,7 @@ namespace webNET_Hits_backend_aspnet_project_2.Data
     {
         public DbSet<UserData> Users { get; set; }
         public DbSet<PasswordModel> UserPasswords { get; set; }
+        public DbSet<PostData> Posts { get; set; }
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
         {
---
+++ webNET-Hits-backend-aspnet-project-2/Migrations/AppDbContext/20231208173319_Posts.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webNET_Hits_backend_aspnet_project_2.Migrations
+{
+    /// <inheritdoc />
+    public partial class Posts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    ReadingTime = table.Column<int>(type: "integer", nullable: false),
+                    Image = table.Column<string>(type: "text", nullable: true),
+                    AuthorId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CommunityId = table.Column<Guid>(type: "uuid", nullable: true),
+                    AddressId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
