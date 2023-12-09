c29d93a0318f1ebaa6b53d0b39d281635c05d7e7
Gegelman Roman Yurievich <gegelmanr@mail.ru>
2023-12-09 15:25:44 +0700

    PostConrtoller was created
    PostService was created and integrated into Program.cs

---
+++ webNET-Hits-backend-aspnet-project-2/Controllers/PostController.cs
@@ -0,0 +1,31 @@
+﻿using webNET_Hits_backend_aspnet_project_2.Models.EnumModels;
+using webNET_Hits_backend_aspnet_project_2.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace webNET_Hits_backend_aspnet_project_2.Controllers
+{
+    [ApiController]
+    [Route("api/post")]
+    public class PostController : Controller
+    {
+        private readonly PostService _postService;
+        private readonly UsersService _userService;
+        private readonly AddressService _addressService;
+        private readonly TagService _tagService;
+        private readonly IConfiguration _configuration;
+
+        public PostController(IConfiguration configuration, PostService postService, UsersService userService, AddressService addressService, TagService tagService)
+        {
+            _configuration = configuration;
+            _postService = postService;
+            _userService = userService;
+            _addressService = addressService;
+            _tagService = tagService;
+        }
+
+
+        
+    }
+}
---
+++ webNET-Hits-backend-aspnet-project-2/Services/PostService.cs
@@ -0,0 +1,20 @@
+﻿using webNET_Hits_backend_aspnet_project_2.Data;
+using webNET_Hits_backend_aspnet_project_2.Models.DbModels;
+using webNET_Hits_backend_aspnet_project_2.Models.EnumModels;
+using Microsoft.Extensions.Hosting;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace webNET_Hits_backend_aspnet_project_2.Services
+{
+    public class PostService
+    {
+        private readonly AppDbContext _dbContext;
+
+        public PostService(AppDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        
+    }
+}
