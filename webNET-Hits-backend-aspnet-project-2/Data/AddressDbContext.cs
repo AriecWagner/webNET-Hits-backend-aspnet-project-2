900cc448f590a37957a79e234c4efd67a7267e59
Gegelman Roman Yurievich <gegelmanr@mail.ru>
2023-12-09 13:22:13 +0700

    AddressDbContext was created
    GAR database data can now be used. Models and Migrations

---
+++ webNET-Hits-backend-aspnet-project-2/Data/AddressDbContext.cs
@@ -0,0 +1,19 @@
+﻿using webNET_Hits_backend_aspnet_project_2.Models.DbModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace webNET_Hits_backend_aspnet_project_2.Data
+{
+    public class AddressDbContext : DbContext
+    {
+        public DbSet<AsAdmHierarchy> as_adm_hierarchy { get; set; }
+        public DbSet<AsAddrObj> as_addr_obj { get; set; }
+        public DbSet<AsHouses> as_houses { get; set; }
+
+        public AddressDbContext(DbContextOptions<AddressDbContext> options)
+            : base(options)
+        {
+            //Database.EnsureCreated();
+        }
+    }
+}
---
+++ webNET-Hits-backend-aspnet-project-2/Models/DbModels/AsAdmHierarchy.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webNET_Hits_backend_aspnet_project_2.Models.DbModels
+{
+    [Table("as_adm_hierarchy", Schema = "fias")]
+    public class AsAdmHierarchy
+    {
+        [Key]
+        public long id { get; set; }
+        public long objectid { get; set; }
+        public long parentobjid { get; set; }
+        public long changeid { get; set; }
+        public string regioncode { get; set; }
+        public string? areacode { get; set; }
+        public string? citycode { get; set; }
+        public string? placecode { get; set; }
+        public string? plancode { get; set; }
+        public string? streetcode { get; set; }
+        public long previd { get; set; }
+        public long nextid { get; set; }
+        public DateTime updatedate { get; set; }
+        public DateTime startdate { get; set; }
+        public DateTime enddate { get; set; }
+        public int isactive { get; set; }
+        public string path { get; set; }
+    }
+}
