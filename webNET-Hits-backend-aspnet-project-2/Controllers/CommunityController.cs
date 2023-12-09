3ad712e1c95af24d7f98df853f296bbd109b0f1f
Gegelman Roman Yurievich <gegelmanr@mail.ru>
2023-12-10 00:02:54 +0700

    bugs with community was fixed

--- webNET-Hits-backend-aspnet-project-2/Controllers/CommunityController.cs
+++ webNET-Hits-backend-aspnet-project-2/Controllers/CommunityController.cs
@@ -1,11 +1,12 @@
-﻿using webNET_Hits_backend_aspnet_project_2.Models.EnumModels;
+﻿using webNET_Hits_backend_aspnet_project_2.Models.InputModels;
+using webNET_Hits_backend_aspnet_project_2.Models.EnumModels;
 using webNET_Hits_backend_aspnet_project_2.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using webNET_Hits_backend_aspnet_project_2.Models.AnotherModels;
 
-//ДОБАВИТЬ ВОЗМОЖНОСТЬ ПОДПИСАТЬ, БУДУЧИ АДМИНОМ, ПОЛЬЗОВАТЕЛЯ НА ЗАКРЫТУЮ COMMUNITY
+
 //ДОБАВИТЬ ВОЗМОЖНОСТЬ СОЗДАТЬ СВОЮ COMMUNITY
 
 namespace webNET_Hits_backend_aspnet_project_2.Controllers
@@ -31,8 +32,6 @@ namespace webNET_Hits_backend_aspnet_project_2.Controllers
         }
 
         [HttpGet]
-
-
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(string))]
@@ -96,16 +95,6 @@ namespace webNET_Hits_backend_aspnet_project_2.Controllers
                 if (community != null)
                 {
                     return Ok(community);
-
-        public IActionResult GetCommunties()
-        {
-            try
-            {
-                var communities = _communityService.GetCommunities();
-
-                if (communities != null)
-                {
-                    return Ok(communities);
                 }
                 else
                 {
@@ -118,9 +107,6 @@ namespace webNET_Hits_backend_aspnet_project_2.Controllers
             }
         }
 
-
-        [HttpGet("my")]
-
         [HttpGet("{id}/posts")]
         [Authorize]
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -128,34 +114,6 @@ namespace webNET_Hits_backend_aspnet_project_2.Controllers
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(string))]
-        public IActionResult GetUsCommunyList()
-        {
-            try
-            {
-                Guid userId = Guid.Parse(User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value);
-
-                if (!_userService.IsUserAuthenticated(userId, out var errorMessage))
-                {
-                    return BadRequest(new { errorMessage });
-                }
-
-                return Ok(_communityService.GetMembershipsUser(userId));
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, "Произошла ошибка сервера");
-            }
-        }
- GetCommunityEndpoints
-        [HttpGet("{id}/posts")]
-        [Authorize]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(string))]
-
- GetCommunityEndpoints
         public IActionResult GetCommunityPosts(
             Guid id,
             [FromQuery] List<Guid>? tags,
@@ -265,7 +223,6 @@ namespace webNET_Hits_backend_aspnet_project_2.Controllers
             }
         }
 
- GetCommunityEndpoints
         [HttpGet("{id}/role")]
         [Authorize]
         [ProducesResponseType(StatusCodes.Status200OK)]
