701075d0bb90af258903b87fbb1ff2f2f8aa94ce
Gegelman Roman Yurievich <gegelmanr@mail.ru>
2023-12-09 18:18:49 +0700

    Edit comment was added
    Functions for CommentAlreadyDeleted, EditComment in comment service was added
    in controller was added validation for check existence  cooment, check Authenticated and chek content of comment

--- webNET-Hits-backend-aspnet-project-2/Controllers/CommentController.cs
+++ webNET-Hits-backend-aspnet-project-2/Controllers/CommentController.cs
@@ -24,5 +24,60 @@ namespace webNET_Hits_backend_aspnet_project_2.Controllers
             _postService = postService;
             _commentService = commentService;
         }
+
+        [HttpPut("{id}")]
+        [Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(string))]
+        public async Task<IActionResult> EditComment([FromBody] CommentContent comment, Guid id)
+        {
+            try
+            {
+                Guid userId = Guid.Parse(User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value);
+
+                if (!_userService.IsUserAuthenticated(userId, out var errorMessage))
+                {
+                    return BadRequest(new { errorMessage });
+                }
+
+                if (comment.Content == null || comment.Content == "")
+                {
+                    return BadRequest("Вы отредактировали комментарий до удаления:)");
+                    //return BadRequest("Комментарий пуст");
+                }
+
+                if (comment.Content.Length > 1000)
+                {
+                    return BadRequest("Короче изъясняйся, ежи");
+                    //return BadRequest("Комментарий пуст");
+                }
+
+                if (!_commentService.CommentExists(id))
+                {
+                    return BadRequest("Такого комментария не существует");
+                }
+
+                if (_commentService.CommentAlreadyDeleted(id))
+                {
+                    return BadRequest("Нельзя изменить то, чего нет");
+                }
+
+                _commentService.EditComment(comment.Content, id);
+                return Ok("Комментарий успешно отредактирован");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                if (ex.InnerException != null)
+                {
+                    Console.WriteLine($"Inner exception: {ex.InnerException.Message}");
+                }
+                // Добавьте другие сведения о исключении при необходимости
+                throw;
+            }
+        }
     }
 }
