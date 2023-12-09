afce38de6474c4453bb6ba6b96c62bbe1862e1a2
Gegelman Roman Yurievich <gegelmanr@mail.ru>
2023-12-09 23:42:01 +0700

    Community DTO model was added

---
+++ webNET-Hits-backend-aspnet-project-2/Models/DTOModels/CommunityMemberDTO.cs
@@ -0,0 +1,16 @@
+﻿using webNET_Hits_backend_aspnet_project_2.Models.EnumModels;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace webNET_Hits_backend_aspnet_project_2.Models.AnotherModels
+{
+    public class CommunityMemberDTO
+    {
+        public Guid UserId { get; set; }
+        public Guid CommunityId { get; set; }
+        [Column("CommunityRole", TypeName = "text")]
+        [EnumDataType(typeof(CommunityRole))]
+        public CommunityRole Role { get; set; }
+    }
+}
