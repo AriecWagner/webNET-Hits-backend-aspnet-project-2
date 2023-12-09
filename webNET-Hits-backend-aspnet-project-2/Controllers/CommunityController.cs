8a5666388450fb3ee71b1aadb4c66b2ef7c87fc3
Gegelman Roman Yurievich <gegelmanr@mail.ru>
2023-12-09 23:17:15 +0700

    Community role endpoint was added
    function GetTheGreatestRole was created

--- webNET-Hits-backend-aspnet-project-2/Controllers/CommunityController.cs
+++ webNET-Hits-backend-aspnet-project-2/Controllers/CommunityController.cs
@@ -31,5 +31,30 @@ namespace webNET_Hits_backend_aspnet_project_2.Controllers
             _tagService = tagService;
         }
 
+        [HttpGet("{id}/role")]
+        [Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(string))]
+        public IActionResult GetGreatestUsRole(Guid id)
+        {
+            try
+            {
+                Guid userId = Guid.Parse(User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value);
+
+                if (!_userService.IsUserAuthenticated(userId, out var errorMessage))
+                {
+                    return BadRequest(new { errorMessage });
+                }
+
+                return Ok(_communityService.GetTheGreatestRole(id, userId));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Произошла ошибка сервера");
+            }
+        }
     }
 }
--- webNET-Hits-backend-aspnet-project-2/Services/CommunityService.cs
+++ webNET-Hits-backend-aspnet-project-2/Services/CommunityService.cs
@@ -15,7 +15,17 @@ namespace webNET_Hits_backend_aspnet_project_2.Services
         {
             _dbContext = context;
         }
-        
-       
+
+        public CommunityRole? GetTheGreatestRole(Guid communityId, Guid userId)
+        {
+            var member = _dbContext.CommunityMembers.Where(c => c.UserId == userId && c.CommunityId == communityId).ToList();
+
+            bool isAdmin = member.Exists(m => m.Role == CommunityRole.Administrator);
+            bool isSubscriber = member.Exists(m => m.Role == CommunityRole.Subscriber);
+
+            if (isAdmin) return CommunityRole.Administrator;
+            if (isSubscriber) return CommunityRole.Subscriber;
+            return null;
+        }
     }
 }
