7ee4d3bc3f7ba318a7bed4c1076f95edaca616f2
Gegelman Roman Yurievich <gegelmanr@mail.ru>
2023-12-09 18:42:16 +0700

    Community controller was created

---
+++ webNET-Hits-backend-aspnet-project-2/Controllers/CommunityController.cs
@@ -0,0 +1,35 @@
+﻿using webNET_Hits_backend_aspnet_project_2.Models.EnumModels;
+using webNET_Hits_backend_aspnet_project_2.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+//ДОБАВИТЬ ВОЗМОЖНОСТЬ ПОДПИСАТЬ, БУДУЧИ АДМИНОМ, ПОЛЬЗОВАТЕЛЯ НА ЗАКРЫТУЮ COMMUNITY
+//ДОБАВИТЬ ВОЗМОЖНОСТЬ СОЗДАТЬ СВОЮ COMMUNITY
+
+namespace webNET_Hits_backend_aspnet_project_2.Controllers
+{
+    [ApiController]
+    [Route("api/community")]
+    public class CommunityController : Controller
+    {
+        private readonly CommunityService _communityService;
+        private readonly PostService _postService;
+        private readonly UsersService _userService;
+        private readonly AddressService _addressService;
+        private readonly TagService _tagService;
+        private readonly IConfiguration _configuration;
+
+
+        public CommunityController(IConfiguration configuration, CommunityService communityService, PostService postService, UsersService userService, AddressService addressService, TagService tagService)
+        {
+            _configuration = configuration;
+            _communityService = communityService;
+            _postService = postService;
+            _userService = userService;
+            _addressService = addressService;
+            _tagService = tagService;
+        }
+
+    }
+}
---
+++ webNET-Hits-backend-aspnet-project-2/Services/CommunityService.cs
@@ -0,0 +1,21 @@
+﻿using webNET_Hits_backend_aspnet_project_2.Data;
+using webNET_Hits_backend_aspnet_project_2.Models.DbModels;
+using webNET_Hits_backend_aspnet_project_2.Models.EnumModels;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace webNET_Hits_backend_aspnet_project_2.Services
+{
+    public class CommunityService
+    {
+        private readonly AppDbContext _dbContext;
+
+        public CommunityService(AppDbContext context)
+        {
+            _dbContext = context;
+        }
+        
+       
+    }
+}
