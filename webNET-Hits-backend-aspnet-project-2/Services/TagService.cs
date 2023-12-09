24a44476cbca4d7dd9bf4cd4f580918e0dfa7952
Gegelman Roman Yurievich <gegelmanr@mail.ru>
2023-12-09 15:12:43 +0700

    function for check correct tag was added

--- webNET-Hits-backend-aspnet-project-2/Services/TagService.cs
+++ webNET-Hits-backend-aspnet-project-2/Services/TagService.cs
@@ -19,6 +19,35 @@ namespace webNET_Hits_backend_aspnet_project_2.Services
             return _dbContext.Tags.ToList();
         }
 
+        public bool CheckIsCorrectTags(List<Guid>? tags)
+        {
+            if (tags == null)
+            {
+                return true;
+            }
 
+            var tagsList = _dbContext.Tags.ToList();
+            foreach (var tag in tags)
+            {
+                bool flag = false;
+
+                foreach (var tagFromTagList in tagsList)
+                {
+                    if (tag == tagFromTagList.Id)
+                    {
+                        tagsList.Remove(tagFromTagList);
+                        flag = true;
+                        break;
+                    }
+                }
+
+                if (!flag)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
