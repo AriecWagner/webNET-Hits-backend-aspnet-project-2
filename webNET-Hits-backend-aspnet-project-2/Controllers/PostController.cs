326a33fda03a418075d8308fce0e33fec5de6309
Gegelman Roman Yurievich <gegelmanr@mail.ru>
2023-12-09 16:00:53 +0700

    Posting post endpoint was added
    also input model for this endpoint was added and function, that added a new post to db

--- webNET-Hits-backend-aspnet-project-2/Controllers/PostController.cs
+++ webNET-Hits-backend-aspnet-project-2/Controllers/PostController.cs
@@ -25,7 +25,63 @@ namespace webNET_Hits_backend_aspnet_project_2.Controllers
             _tagService = tagService;
         }
 
+        [HttpPost]
+        [Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(string))]
+        public async Task<IActionResult> CreateUsPersPost([FromBody] CreatePostModel userPost)
+        {
+            try
+            {
+                Guid userId = Guid.Parse(User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value);
 
-        
+                if (!_userService.IsUserAuthenticated(userId, out var errorMessage))
+                {
+                    return BadRequest(new { errorMessage });
+                }
+
+                if (userPost.Title == null)
+                {
+                    return BadRequest("Название поста должно БЫТЬ!!!");
+                }
+
+                if (userPost.Description == null)
+                {
+                    return BadRequest("Пост должен ИМЕТЬ описание!!!");
+                }
+
+                if (userPost.ReadingTime == null || userPost.ReadingTime < 0)
+                {
+                    return BadRequest("А сколько читать ваш пост???");
+                }
+
+                if (!_addressService.checkIsCorrectAddress(userPost.AddressId))
+                {
+                    return BadRequest("Адрес некоректный");
+                }
+
+                if (!_tagService.CheckIsCorrectTags(userPost.Tags))
+                {
+                    return BadRequest("У вас не те теги");
+                }
+
+                _postService.CreatePersonalUserPost(userPost, userId);
+                return Ok("Пост успешно создан");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                if (ex.InnerException != null)
+                {
+                    Console.WriteLine($"Inner exception: {ex.InnerException.Message}");
+                }
+                // Добавьте другие сведения о исключении при необходимости
+                throw;
+            }
+        }
+
     }
 }
--- webNET-Hits-backend-aspnet-project-2/Services/PostService.cs
+++ webNET-Hits-backend-aspnet-project-2/Services/PostService.cs
@@ -3,6 +3,7 @@ using webNET_Hits_backend_aspnet_project_2.Models.DbModels;
 using webNET_Hits_backend_aspnet_project_2.Models.EnumModels;
 using Microsoft.Extensions.Hosting;
 using static System.Net.Mime.MediaTypeNames;
+using webNET_Hits_backend_aspnet_project_2.Models.AnotherModels;
 
 namespace webNET_Hits_backend_aspnet_project_2.Services
 {
@@ -15,6 +16,43 @@ namespace webNET_Hits_backend_aspnet_project_2.Services
             _dbContext = context;
         }
 
-        
+        public PostData? CreatePersonalUserPost(CreatePostModel post, Guid authorId)
+        {
+            Guid postId = Guid.NewGuid();
+
+            PostData fullPost = new PostData
+            {
+                Id = postId,
+                CreateTime = DateTime.UtcNow,
+                Title = post.Title,
+                Description = post.Description,
+                ReadingTime = post.ReadingTime,
+                Image = post.Image,
+                AuthorId = authorId,
+                CommunityId = null,
+                AddressId = post.AddressId
+            };
+
+            if (post.Tags != null && post.Tags.Count > 0)
+            {
+                foreach (var tag in post.Tags)
+                {
+                    TagPostJoin tagPostJoin = new TagPostJoin
+                    {
+                        Id = new Guid(),
+                        PostId = postId,
+                        TagId = tag
+                    };
+                    _dbContext.TagsPostsJoinTable.Add(tagPostJoin);
+                }
+            }
+
+
+            _dbContext.Posts.Add(fullPost);
+            _dbContext.SaveChanges();
+
+            return fullPost;
+        }
+
     }
 }
