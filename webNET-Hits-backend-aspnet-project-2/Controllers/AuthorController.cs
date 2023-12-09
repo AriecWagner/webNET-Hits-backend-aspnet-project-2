c88004e77145a57681ddb3cbfb359101fd1ef500
Gegelman Roman Yurievich <gegelmanr@mail.ru>
2023-12-09 15:18:16 +0700

    Logic of author controller was added
    DTO model of author, function for getting this DTO were added

--- webNET-Hits-backend-aspnet-project-2/Controllers/AuthorController.cs
+++ webNET-Hits-backend-aspnet-project-2/Controllers/AuthorController.cs
@@ -1,5 +1,6 @@
 ﻿using webNET_Hits_backend_aspnet_project_2.Services;
 using Microsoft.AspNetCore.Mvc;
+using webNET_Hits_backend_aspnet_project_2.Models.AnotherModels;
 
 namespace webNET_Hits_backend_aspnet_project_2.Controllers
 {
@@ -15,6 +16,30 @@ namespace webNET_Hits_backend_aspnet_project_2.Controllers
         {
             _configuration = configuration;
             _authorService = authorService;
+        }
+
+        [HttpGet("list")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(string))]
+        public async Task<IActionResult> GetAuthorsList()
+        {
+            try
+            {
+                List<AuthorDTO> authors = _authorService.GetAuthorDTOs();
+                return Ok(authors);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                if (ex.InnerException != null)
+                {
+                    Console.WriteLine($"Inner exception: {ex.InnerException.Message}");
+                }
+                // Добавьте другие сведения о исключении при необходимости
+                throw;
+            }
         }
     }
 }
---
+++ webNET-Hits-backend-aspnet-project-2/Models/DTOModels/AuthorDTO.cs
@@ -0,0 +1,19 @@
+﻿using webNET_Hits_backend_aspnet_project_2.Models.EnumModels;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace webNET_Hits_backend_aspnet_project_2.Models.AnotherModels
+{
+    public class AuthorDTO
+    {
+        public string FullName { get; set; }
+        public DateTime BirthDate { get; set; }
+        [Column("Gender", TypeName = "text")]
+        [EnumDataType(typeof(Gender))]
+        public Gender Gender { get; set; }
+        public int Posts { get; set; }
+        public int Likes { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
