9a7c2ffdd906565cfea370babb08e381cd4f3df1
Gegelman Roman Yurievich <gegelmanr@mail.ru>
2023-12-09 18:08:10 +0700

    Creating comment endpoint was added
    Check post existence function in PostService was added
    Comment input model was added
    GetCommentById, checkParentIsCorrect, AddCommentToPost funcrions in comment service was added

--- webNET-Hits-backend-aspnet-project-2/Services/CommentService.cs
+++ webNET-Hits-backend-aspnet-project-2/Services/CommentService.cs
@@ -1,6 +1,7 @@
 ﻿using webNET_Hits_backend_aspnet_project_2.Data;
 using webNET_Hits_backend_aspnet_project_2.Models.DbModels;
 using Microsoft.Extensions.Hosting;
+using webNET_Hits_backend_aspnet_project_2.Models.AnotherModels;
 
 namespace webNET_Hits_backend_aspnet_project_2.Services
 {
@@ -11,6 +12,39 @@ namespace webNET_Hits_backend_aspnet_project_2.Services
         public CommentService(AppDbContext context)
         {
             _dbContext = context;
+        }
+
+        public CommentModel? GetCommentById(Guid? id)
+        {
+            return _dbContext.Comments.FirstOrDefault(item => item.Id == id);
+        }
+
+        public bool checkParentIsCorrect(Guid? id)
+        {
+            if (id == Guid.Empty) { return true; }
+
+            var answer = GetCommentById(id);
+
+            if (answer != null) { return true; }
+            else { return false; }
+        }
+
+        public void AddCommentToPost(CommentInput commentInput, Guid postId, Guid userId)
+        {
+            CommentModel comment = new CommentModel
+            {
+                Id = new Guid(),
+                Content = commentInput.Content,
+                PostId = postId,
+                ParentId = commentInput.ParentId,
+                AuthorId = userId,
+                CreateTime = DateTime.UtcNow,
+                ModifiedDate = null,
+                DeleteDate = null
+            };
+
+            _dbContext.Comments.Add(comment);
+            _dbContext.SaveChanges();
         }
     }
 }
