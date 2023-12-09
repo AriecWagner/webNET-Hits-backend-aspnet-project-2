f4d6624b8c5a64f95137ed872fa2e18425488575
Gegelman Roman Yurievich <gegelmanr@mail.ru>
2023-12-09 14:36:34 +0700

    address controller and service were created

---
+++ webNET-Hits-backend-aspnet-project-2/Controllers/AddressController.cs
@@ -0,0 +1,52 @@
+﻿using backtestDevelop.Models;
+using backtestDevelop.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace backtestDevelop.Controllers
+{
+    [ApiController]
+    [Route("api/address")]
+    public class AddressController : ControllerBase
+    {
+        private readonly AddressService _addressService;
+
+        public AddressController(AddressService addressService)
+        {
+            _addressService = addressService;
+        }
+
+        [HttpGet("search")]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(string))]
+        public IActionResult Search(int parentObjectId, string? query)
+        {
+            try
+            {
+                var response = _addressService.SearchAddress(parentObjectId, query);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Произошла ошибка сервера " + ex.ToString());
+            }
+        }
+
+        [HttpGet("chain")]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(string))]
+        public IActionResult SearchChain(string objectGuid)
+        {
+            try
+            {
+                var response = _addressService.SearchAddressChain(Guid.Parse(objectGuid));
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Произошла ошибка сервера " + ex.ToString());
+            }
+        }
+    }
+
+}
---
+++ webNET-Hits-backend-aspnet-project-2/Services/AddressService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using backtestDevelop.Data;
+using backtestDevelop.Models.AnotherModels;
+using backtestDevelop.Models.DbModels;
+using backtestDevelop.Models.EnumModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace backtestDevelop.Services
+{
+    public class AddressService
+    {
+        private readonly AddressDbContext _dbContext;
+
+        public AddressService(AddressDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
